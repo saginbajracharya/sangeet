b6c76ccf9d0a9f06e49b8918042455331d9c9bda
saginbajracharya <saginbajracharya09@gmail.com>
2020-11-18 12:23:41 +0545

    Dark mode and light mode

---
+++ lib/src/helper/constants.dart
@@ -0,0 +1,5 @@
+abstract class SharedPreferencesKeys {
+  static const String isDarkTheme = 'isDarkTheme';
+  static const String homeCountryDetails = 'homeCountry';
+}
+
---
+++ lib/src/widgets/themeSwitch.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:sangeet/src/helper/constants.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../main.dart';
+
+class ThemeSwitch extends StatefulWidget {
+  @override
+  _ThemeSwitchState createState() => _ThemeSwitchState();
+}
+
+class _ThemeSwitchState extends State<ThemeSwitch> {
+  var _darkTheme = true;
+
+  void onThemeChanged(bool value, ThemeNotifier themeNotifier) async {
+    (value)
+        ? themeNotifier.setTheme(darkTheme)
+        : themeNotifier.setTheme(lightTheme);
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool(SharedPreferencesKeys.isDarkTheme, value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+    _darkTheme = (themeNotifier.getTheme() == darkTheme);
+    return Switch(
+      activeColor: Theme.of(context).accentColor,
+      onChanged: (value) {
+        setState(() {
+          _darkTheme = value;
+          print(_darkTheme);
+          print("true->Dark || False->Light");
+        });
+        onThemeChanged(value, themeNotifier);
+      },
+      value: _darkTheme,
+    );
+  }
+}
