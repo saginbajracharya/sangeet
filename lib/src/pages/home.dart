b977e8accd228471ef52b0d05b6123a988cad1b3
ashok_shk <ashok.shk01@gmail.com>
2020-11-18 14:31:33 +0545

    [Added :: Play/Play and previous / next buttons]

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -4,7 +4,6 @@ import 'package:sangeet/src/widgets/themeSwitch.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
-
   final String title;
 
   @override
@@ -12,7 +11,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,9 +24,68 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ThemeSwitch(),
+            bottomPanel()
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget bottomPanel() {
+    return Column(children: <Widget>[
+      // Padding(
+      //   padding: EdgeInsets.symmetric(horizontal: 16),
+      //   child: songProgress(context),
+      // ),
+      Container(
+        padding: EdgeInsets.symmetric(vertical: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            CircleAvatar(
+              child: Center(
+                child: IconButton(
+                    icon: Icon(
+                      Icons.skip_previous,
+                      color: Colors.white,
+                    ),
+                    onPressed: () => print('Previous')
+                ),
+              ),
+              backgroundColor: Colors.cyan.withOpacity(0.3),
+            ),
+            CircleAvatar(
+              radius: 30,
+              child: Center(
+                child: IconButton(
+                  onPressed: () async {
+                    print('Play / Pause');
+                  },
+                  padding: const EdgeInsets.all(0.0),
+                  icon: Icon(
+                    // audioManagerInstance.isPlaying
+                        /* ? Icons.pause
+                        : */ Icons.play_arrow,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.cyan.withOpacity(0.3),
+              child: Center(
+                child: IconButton(
+                    icon: Icon(
+                      Icons.skip_next,
+                      color: Colors.white,
+                    ),
+                    onPressed: () => print('Next')
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ]);
   }
 }
--- lib/src/widgets/sidebar.dart
+++ lib/src/widgets/sidebar.dart
@@ -19,7 +19,7 @@ class SideBar extends StatelessWidget{
           ),
           ListTile(
             leading: Icon(Icons.music_note),
-            title: Text('List All'),
+            title: Text('All Songs'),
             onTap: () => {},
           ),
           ListTile(
